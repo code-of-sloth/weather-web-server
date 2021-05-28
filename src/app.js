a27472de84872572aab327c0c844d4f8257cddb3
Richard <richard9343@gmail.com>
2021-05-28 20:45:49 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path=require('path')
+const hbs=require('hbs')
+const app = express()
+const port = 3000
+const {weather}=require('./weather')
+
+const pathDir=path.join(__dirname,'../public')
+const viewPath=path.join(__dirname,'../views')
+const partialsPath=path.join(__dirname,'../partials')
+
+
+app.set('view engine', 'hbs');
+app.set('views',viewPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(pathDir))
+
+
+app.get('/',(req,res)=>res.render('index',{title:'Index',name:'Richard'}))
+app.get('/help',(req,res)=>res.render('help',{title:'Help',name:'Richard'}))
+app.get('/about',(req,res)=>res.render('about',{title:'About',name:'Richard'}))
+app.get('/weather',({query}={},res)=> {
+    weather(query.place,res,(climate,time,error)=>{
+        if(error)
+            {
+                return res.send([{error:`couldn't connect to weatherstack`}]);
+            } 
+            else if(!(climate && time))
+            {
+                return res.send([{error:`Enter valid location`}])
+            }
+            else
+            {
+                return res.send([{place:query.place,time,climate:`${climate}`}]);
+            }
+    })
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/weather.js
@@ -0,0 +1,35 @@
+const request=require('request');
+
+
+function weather(place,res,callback) {
+    // console.log(place);
+    location(place,(data,error)=>{
+        if(error)
+        {
+            return res.send([{error:'Could not get location'}]);
+        } else if(data.features.length<1)
+        {
+            return res.send([{error:'Enter valid location'}]);
+        } else
+        {
+            const info=[data.features[0].center[1],data.features[0].center[0],data.features[0].place_name];
+            const url=`http://api.weatherstack.com/current?access_key=4e8613fc875752db4643f7b40c49a3ea&query=${info[0]},${info[1]}`;
+            request({url:url,json:true},(error,response)=>{
+                callback(response.body.current.weather_descriptions,response.body.current.observation_time,error)
+                // console.log();
+            })
+        }
+    })
+    
+}
+
+function location(place,callback) {
+    place=place.replace(' ','%20')
+    const url="https://api.mapbox.com/geocoding/v5/mapbox.places/"+place+".json?access_token=pk.eyJ1IjoiZ2F1c3MxMDUiLCJhIjoiY2twMWU5NjE1MWFyYjJvbzJpOTJ1dHoydSJ9.nofLkFFlNLVa9SilBZbK_w"
+    request({url:url,json:true},(error,response)=>{
+        // const data=[response.body.features[0].center[1],response.body.features[0].center[0],response.body.features[0].place_name];
+        callback(response.body,error)
+    })
+}
+
+module.exports={weather,location}
